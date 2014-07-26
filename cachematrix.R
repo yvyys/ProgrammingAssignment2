c399369e003fa954d963b0121e58fe0dfc237c65
yvyys <yves.ndjoli@gmail.com>
2014-07-26 18:58:52 +0200

    Function to perform the calcul from inverse's matrix by cache's using

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## YN
+## The both functions help to perform the calculation
+## of the matrix's inverse by cache's using
 
-## Write a short comment describing this function
-
+## Save the values from the matrix and the function, they
+## will be cache
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
-
+## Check if the inverse from the matrix exist in cache
+## if yes: text plus value of the inverse
+## if no: calculate to value from the inverse and save
+## the value in the cache 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached inverse")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m
 }
+
